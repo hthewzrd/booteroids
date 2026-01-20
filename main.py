fb796ad3e2a3ec78aeb8aa2b6773e9d60f2ca120
hthewzrd <dhund.data@gmail.com>
2026-01-20 21:35:32 +0000

    booteroids tutorial reupload to new github

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# test command : uv run main.py
 import pygame
 from constants import *
 from player import Player
@@ -28,7 +29,6 @@ def main():
 
         # 60 fps
         dt = clock.tick(60) / 1000
-
     
 if __name__ == "__main__":
     main()
