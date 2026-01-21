b0c3ba22133dfc13c293441781a9d0fac746ac55
hthewzrd <dhund.data@gmail.com>
2026-01-21 12:11:24 +0000

    Added rotation to player

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # test command : uv run main.py
 import pygame
-from constants import *
 from player import Player
-
+from constants import *
+from logger import log_state
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
     clock = pygame.time.Clock()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     # delta time for fps
@@ -19,16 +21,17 @@ def main():
 
     running = True
     while running:
+        log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        screen.fill("black")
-        player.draw(screen)
-        pygame.display.flip()
 
         # 60 fps
         dt = clock.tick(60) / 1000
-    
+        player.update(dt)
+
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
 if __name__ == "__main__":
     main()
