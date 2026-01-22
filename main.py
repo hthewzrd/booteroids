3a270f9829294f77b72c73a9945575984b0dcb0f
hthewzrd <dhund.data@gmail.com>
2026-01-22 15:07:08 +0000

    asteroids and collision detection added and working

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # test command : uv run main.py
+import sys 
+
 import pygame
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
-from logger import log_state
+from logger import log_event, log_state
 
 def main():
     pygame.init()
@@ -10,9 +14,18 @@ def main():
 
 
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     # delta time for fps
     dt = 0
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    Asteroid.containers = (asteroids, updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -26,12 +39,21 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+
         # 60 fps
         dt = clock.tick(60) / 1000
-        player.update(dt)
-
+        
         screen.fill("black")
-        player.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
+        
 if __name__ == "__main__":
     main()
